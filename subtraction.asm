2794cff43d8df7a1425059e2c904d951fdfefd28
Rayhan Islam Prome <rayhanspydet@gmail.com>
2025-10-06 06:55:23 +0600

    subtraction.asm

--- subtraction.asm
+++ subtraction.asm
@@ -1,17 +1,17 @@
 .model small          ; memory model
-.stack 100h           ; stack size 256 bytes
+.stack 100h           ; Reserve 256 bytes for the stack
 
 .data                 
-    num1 db 25        ; first number define byte
-    num2 db 7         ; second number define byte
+    num1 db 25        ; First number (in decimal 25
+    num2 db 7         ; Second number (in decimal 7)
 
 .code                 
 start:                
-    mov ax, @data     ; load data segment
-    mov ds, ax        ; set DS
+    mov ax, @data     ; Load address of data segment into AX
+    mov ds, ax        ; Initialize DS register (data segment)
 
-    mov al, num1      ; AL = num1
-    sub al, num2      ; AL = num1 - num2
+    mov al, num1      ;Move first number into AL
+    sub al, num2      ; Subtract num2 from num1 → AL = num1 - num2
 
     add al, '0'       ; convert to ASCII
     mov dl, al        ; move to DL
@@ -19,6 +19,7 @@ start:
     int 21h           
 
     mov ah, 4Ch       ; exit program
-    int 21h           
+    int 21h           ;Call DOS interrupt to print the result
 
-end start             ; end
+
+end start             ;End of program
